94cdd5a41ecd94763f0f4891ce9cf5d748201dbf
Ganesh  Salunkhe <ganeshsalunkhe1998@gmail.com>
2025-01-03 10:44:56 +0530

    styled component library learnt

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import GlobalStyles from './styles/GlobalStyles';
+import Heading from './ui/Heading';
+import Input from './ui/Input';
 function App() {
 
   return (
     <>
-    <h1>Hi</h1>
+      <GlobalStyles />
+      <Heading as='h1'>The wild oasis</Heading>
+      <Heading as='h2'>The check in and check out</Heading>
+
+      <Input type="number" placeholder="Number of guests" />
+      <Heading as='h3'>Form</Heading>
     </>
-  )
+  );
 }
 
 export default App
